d8a0db31d017285c00ece209f0a2653ea3ac0b71
Ahsan Zia <Ahsan97@hotmail.co.uk>
2020-07-03 21:39:21 +0100

    started project

---
+++ main.py
@@ -0,0 +1,7 @@
+#The number of lights should be configurable on the command line with a default value of 20.
+#The set of lights will be made up of a repeating set of colours in a fixed order (e.g red, green, white)
+#When you run the program, it should turn each light in the set on for 1 second and then turn it off.
+#The output of the program should be formatted as follows: {HH:MM:SS} {light no / postion} {colour} Light {light state}
+#12:00:01: 1 Red Light On 12:00:02: 1 Red Light Off 12:00:02: 2 Green Light On 12:00:03: 2 Green Light Off 12:00:03: 3 White Light On 12:00:04: 3 White Light Off etc. etc.
+#The sequence should continue in a loop until the program is interrupted.
+#Design the program so that it would be easy to modify the colours and the number of colours that make up the set of lights using object oriented techniques. (e.g. blue, red, yellow, white).
