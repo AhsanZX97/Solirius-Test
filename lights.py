0c514bacf00a881942878243b2708f02a30efcc5
Ahsan Zia <Ahsan97@hotmail.co.uk>
2020-07-06 22:13:08 +0100

    added comments

--- lights.py
+++ lights.py
@@ -1,15 +1,19 @@
 class Lights:
+    # constructor for the class
     def __init__(self, colour):
         self._colour = colour
         self._state = False
 
+    # returns a string to represent the variables of the light e.g Red Light On
     def __str__(self):
         return "{} Light {}".format(self._colour, "Off" if not self._state else "On")
     
+    # sets variable colour of the object
     def set_colour(self, colour):
         self._colour = colour
     
+    # flips the switch between true and false
     def toggleState(self):
-        self._state = True if not self._state else False
+        self._state = not self._state
 
     colour = property(set_colour)
--- main.py
+++ main.py
@@ -10,21 +10,35 @@ from lights import Lights
 from time import gmtime, strftime
 import time
 
+# outputs state of light
 def printLight(light,pos):
     print("{} {} {}".format(strftime("%H:%M:%S", gmtime()),pos,light))#
 
+# The list of colours a light can take up
+Colours = ["Red","Green","White","Blue","Yellow", "Purple", "Orange"]
+
 if __name__ == "__main__":
-    if not sys.argv[1].isdigit():
-        print("please enter a number or leave the argument blank")
-        exit() 
-    Colours = ["Red","Green","White"]
+    # error checks to see if the argument given was a number and not a string
+    if len(sys.argv) > 1:
+        if not sys.argv[1].isdigit():
+            print("please enter a number or leave the argument blank")
+            exit() 
+    
+    # initialise the light set
     LightSet = []
+
+    # if argument wasn't passed, the list is of length 20 otherwise the argument
     maxSet = int(sys.argv[1]) if len(sys.argv) > 1 else 20
+
+    # fills the list with the order of Colour list and turns them on for 1 second
     for i in range(0, maxSet):
-        LightSet.append(Lights(Colours[i%3]))
+        LightSet.append(Lights(Colours[i%7]))
         LightSet[i].toggleState()
         printLight(LightSet[i],i+1)
+    
     time.sleep(1)
+
+    # Goes through the list of lights and turns them off 
     for i in range(0,maxSet):
         LightSet[i].toggleState()
         printLight(LightSet[i],i+1)
@@ -32,9 +46,14 @@ if __name__ == "__main__":
     index = 0
     while True:
         try:
+            # user can enter commands for the program while its running
             string = input("Enter command: ")
+            
+            # closes the program
             if string == "exit":
                 break
+            
+            # runs throught the list of light switching them on and off
             elif string == "run":
                 try: 
                     while True: 
@@ -47,10 +66,12 @@ if __name__ == "__main__":
                         index = 0 if index + 1 == maxSet else index + 1
                 except KeyboardInterrupt:
                     pass
+            
+            # lets a user change the colour of a light, colour has to be in capitale.g set 1 Yellow
             elif string.startswith("set "):
                 req = string[4:].strip()
                 reqList = req.split() 
-                if len(reqList) != 2 or not reqList[0].isdigit() or not isinstance(reqList[1], str): 
+                if len(reqList) != 2 or not reqList[0].isdigit() or not isinstance(reqList[1], str) or reqList[1] not in Colours: 
                     print("please enter a correct format")
                     pass  
                 else:
